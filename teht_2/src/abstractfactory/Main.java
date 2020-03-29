d2e38945a3b6e5237452c177c636d74fa578892e
qqmss <47595921+qqmss@users.noreply.github.com>
2020-03-29 21:09:42 +0300

    teht_2 bonus

--- teht_2/src/abstractfactory/Main.java
+++ teht_2/src/abstractfactory/Main.java
@@ -1,12 +1,37 @@
 package abstractfactory;
 
+import java.io.FileReader;
+import java.util.Properties;
+
 public class Main {
 
     public static void main(String[] args) {
-        Jasper jasper = new Jasper(AdidasFactory.getInstance());
+        Properties properties = new Properties();
+        Class adidasClass = null, bossClass = null;
+        try {
+            properties.load(new FileReader("teht_2/src/abstractfactory/tehdas.properties"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            adidasClass = Class.forName(properties.getProperty("adidas"));
+            bossClass = Class.forName(properties.getProperty("boss"));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Jasper jasper = null;
+        try {
+            jasper = new Jasper((VaateFactory) adidasClass.getMethod("getInstance").invoke(null, null));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println(jasper);
         System.out.println("Jasperista tulee insinööri.");
-        jasper.useVaateFactory(BossFactory.getInstance());
+        try {
+            jasper.useVaateFactory((VaateFactory) bossClass.getMethod("getInstance").invoke(null, null));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println(jasper);
     }
 }
